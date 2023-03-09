4e1b28e864181012a44bc15ca29f2e6dcdd7c9e6
chengguizi <cgz_05@sina.com>
2023-03-09 18:32:42 +0800

    eliminate the use of autopilot time. expose time offset ns to system

--- src/mavsdk/core/include/mavsdk/system.h
+++ src/mavsdk/core/include/mavsdk/system.h
@@ -129,6 +129,8 @@ public:
      */
     enum class ComponentType { UNKNOWN, AUTOPILOT, CAMERA, GIMBAL };
 
+    int64_t get_timesync_offset_ns();
+
     /**
      * @brief type for component discovery callback
      */
--- src/mavsdk/core/system_impl.h
+++ src/mavsdk/core/system_impl.h
@@ -47,6 +47,8 @@ public:
 
     void enable_timesync();
 
+    int64_t get_timesync_offset_ns();
+
     System::IsConnectedHandle subscribe_is_connected(const System::IsConnectedCallback& callback);
     void unsubscribe_is_connected(System::IsConnectedHandle handle);
 
@@ -271,7 +273,7 @@ public:
     bool is_connected() const;
 
     Time& get_time();
-    AutopilotTime& get_autopilot_time() { return _autopilot_time; };
+    // AutopilotTime& get_autopilot_time() { return _autopilot_time; };
 
     double get_ping_time_s() const { return _ping.last_ping_time_s(); }
 
@@ -345,7 +347,7 @@ private:
 
     MavlinkAddress _target_address{};
 
-    AutopilotTime _autopilot_time{};
+    // AutopilotTime _autopilot_time{};
 
     MavlinkMessageHandler _mavlink_message_handler{};
 
